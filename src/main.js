39a1f5d25434283b65a4b5c64d50c52c301b6cf4
Eric Squires <eric.h.squires@gmail.com>
2016-02-07 14:33:55 -0700

    Refactor towards more DRY codebase and implement wait time and duration visualizations

--- src/main.js
+++ src/main.js
@@ -1,51 +1,9 @@
-function organizeSurveyData(chatData) {
-  var organized = [];
-  
-  _.each(chatData, function(chat) {
-    if(!chat.survey_score) {
-      return;
-    }
-    
-    var entry = _.find(organized, function(orgEntry) {
-      return chat.latitude === orgEntry.latitude && chat.longitude === orgEntry.longitude;
-    })
-    
-    if(entry) {
-      entry.total_score += chat.survey_score;
-      entry.total_elements++;
-    }
-    else {
-      organized.push({ latitude: chat.latitude, longitude: chat.longitude, total_score: chat.survey_score, total_elements: 1 });
-    }
-  });
-  
-  var ret = _.flatten(_.map(organized, function(entry) { return [entry.latitude, entry.longitude, entry.total_score / (100 * entry.total_elements)] }));
-  console.log(ret);
-  return ret;
-}
-
-function colorFunction(data) {
-  if(data >= .75) {
-    return new THREE.Color(0x55FF55);
-  }
-  
-  if(data >= .5) {
-    return new THREE.Color(0xDDFFAA);
-  }
-  
-  if(data >= .25) {
-    return new THREE.Color(0xFF5555);
-  }
-  
-  return new THREE.Color(0xFF0000);
-}
-
-function setupGlobe(organizeFun) {
+function setupGlobe(organizeFun, colorFun) {
   // Where to put the globe?
   var container = document.getElementById( 'globe' );
 
   // Make the globe
-  var globe = new DAT.Globe( container, { imgDir: 'img/', colorFn: colorFunction } );
+  var globe = new DAT.Globe( container, { imgDir: 'img/', colorFn: colorFun } );
 
   // We're going to ask a file for the JSON data.
   var xhr = new XMLHttpRequest();
@@ -77,12 +35,94 @@ function setupGlobe(organizeFun) {
 
   // Begin request
   xhr.send( null );
-};
+}
+
+// Perform necessary DOM manipulation for transition to new globe
+function navTo(id) {
+  $('nav .active').removeClass('active');
+  $(id).addClass('active');
+}
+
+function organizeAverage(chatData, prop) {
+  var organized = [];
+  var maxVal = 1;
+  
+  _.each(chatData, function(chat) {
+    // Skip any chat items that didn't record the property
+    if(!chat[prop]) {
+      return;
+    }
+    
+    // Look for an existing entry for the latitude and longitude of the current chat item. If we've already recorded
+    // data for that location, simply update that entry
+    var entry = _.find(organized, function(orgEntry) {
+      return chat.latitude === orgEntry.latitude && chat.longitude === orgEntry.longitude;
+    })
+    
+    if(maxVal < chat[prop]) {
+      maxVal = chat[prop];
+    }
+    
+    if(entry) {
+      entry.total_score += chat[prop];
+      entry.total_elements++;
+    }
+    else {
+      organized.push({ latitude: chat.latitude, longitude: chat.longitude, total_score: chat[prop], total_elements: 1 });
+    }
+  });
+  
+  
+  var ret = _.flatten(_.map(organized, function(entry) {
+    // We divide by maxVal here in order to normalize the data and get it to fit better in the globe visualization
+    return [entry.latitude, entry.longitude, entry.total_score / (maxVal * entry.total_elements)]
+  }));
+  return ret;
+}
+
+// Organizes data for the globe according to the survey scores submitted
+function organizeSurveyData(chatData) {
+  return organizeAverage(chatData, 'survey_score');
+}
+
+// Colors data accordingly for survey data
+function surveyColorFunction(data) {
+  if(data >= .75) {
+    return new THREE.Color(0x55FF55);
+  }
+  
+  if(data >= .5) {
+    return new THREE.Color(0xDDFFAA);
+  }
+  
+  if(data >= .25) {
+    return new THREE.Color(0xFF5555);
+  }
+  
+  return new THREE.Color(0xFF0000);
+}
 
 function gotoSurvey() {
-  $('nav .active').removeClass('active');
-  $('#nav-survey').addClass('active');
-  setupGlobe(organizeSurveyData);
+  navTo('#nav-survey');
+  setupGlobe(organizeSurveyData, surveyColorFunction);
+}
+
+function organizeWaitData(chatData) {
+  return organizeAverage(chatData, 'chat_waittime');
+}
+
+function gotoWaitTime() {
+  navTo('#nav-wait');
+  setupGlobe(organizeWaitData);
+}
+
+function organizeDurationData(chatData) {
+  return organizeAverage(chatData, 'chat_duration');
+}
+
+function gotoDuration() {
+  navTo('#nav-duration');
+  setupGlobe(organizeDurationData);
 }
 
 gotoSurvey();
